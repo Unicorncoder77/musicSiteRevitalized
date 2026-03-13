f4c31f7bd762d95684cfd78634ec9874ca9ecb4a
MusicLibraryFinal <hannanash@go.rmc.edu>
2026-03-13 17:53:42 -0400

    adding urls and items for login

--- the_experience_django/settings.py
+++ the_experience_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -114,9 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'productionfiles'
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, null=True)
+    joined_date = models.DateField(auto_now_add=True, null=True)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, null=True)
+
+class Review(models.Model):
+    review = models.TextField()
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    review_title = models.CharField(max_length=255, null=True)
+    # on_delete = cascade allows for everything from the user or the category to be deleted 
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category_type = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    publication_date = models.DateField(auto_now_add=True, null=True)
+
+class Creator(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True, null=True)
+    pen_name = models.CharField(max_length=255)
+    joined_date = models.DateField(auto_now_add=True, null=True)
+
