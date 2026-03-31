d4039e5878cfbf5a1f707b831b3df9e53cec96ac
unicorncoder77 <hanna.nash19@gmail.com>
2026-03-31 17:33:51 -0400

    updated database with final table items

--- users/models.py
+++ users/models.py
@@ -27,39 +27,43 @@ class Profile(models.Model):
         super().save()
         img = Image.open(self.avatar.path)
 
-        if (img.height > 75 or img.width > 75):
-            newImg = (75, 75)
+        if (img.height > 100 or img.width > 100):
+            newImg = (100, 100)
             img.thumbnail(newImg)
             img.save(self.avatar.path)
 
     # standard str method that returns the username 
     def __str__(self):
         return self.user.username
-
-'''class userPasswords(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    password = models.CharField(max_length=255)'''
-
     
-
-"""
+# one to one would be better for a singular usage not linking a singular key to multiple items
+# the foreign key would work for the rest of these use cases / many-to-one
 class Category(models.Model):
-   category_name = models.CharField(max_length=255, null=True)
+   category_name = models.CharField(max_length=255, null=False)
+
 class Review(models.Model):
     review = models.TextField()
     stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    review_title = models.CharField(max_length=255, null=True)
+    review_title = models.CharField(max_length=255, null=False)
     # on_delete = cascade allows for everything from the user or the category to be deleted 
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    category_type = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    publication_date = models.DateField(auto_now_add=True, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    category_type = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    publication_date = models.DateField(auto_now_add=True, null=False)
 
 class Creator(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True, null=True)
+    email = models.EmailField(max_length=255, unique=True, null=False)
     pen_name = models.CharField(max_length=255)
-    joined_date = models.DateField(auto_now_add=True, null=True)
-"""
+    joined_date = models.DateField(auto_now_add=True, null=False)
+
+class Article(models.Model):
+    article_title = models.CharField(max_length=255)
+    article_contents = models.TextField()
+    creator_id = models.ForeignKey(Creator, on_delete=models.CASCADE, null=False)
+    publication_date = models.DateField(auto_now_add=True, null=False)
+    category_type = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+
+
 
 
